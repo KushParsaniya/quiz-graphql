272a172504767664ccf9b0bca71393bf273e6aa3
Kush Parsaniya <kushparsaniya94@gmail.com>
2023-12-11 18:15:46 +0530

    Project Initialized

---
+++ src/main/java/dev/kush/quizgraphql/QuizGraphqlApplication.java
@@ -0,0 +1,13 @@
+package dev.kush.quizgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kush/quizgraphql/QuizGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kush.quizgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
